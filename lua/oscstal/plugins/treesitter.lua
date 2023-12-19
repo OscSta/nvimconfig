483aa1565af5e6c3dfa28abe7c5fe855e5583c7e
oscsta <oscstal@gmail.com>
2023-12-20 00:10:19 +0100

    treesitter working?

--- lua/oscstal/plugins/treesitter.lua
+++ lua/oscstal/plugins/treesitter.lua
@@ -6,5 +6,19 @@ return {
             'nvim-treesitter/nvim-treesitter-textobjects',
         },
         enabled = true,
+        ensure_installed = {
+            "lua",
+            "rust",
+            "toml",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        ident = { enable = true },
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+        },
     }
 }
