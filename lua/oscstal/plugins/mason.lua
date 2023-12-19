98defa6274f56d73786e55e4131520d0cf51ce0f
oscsta <oscstal@gmail.com>
2023-12-20 00:46:25 +0100

    +rust-tools and fix mason.lua

--- lua/oscstal/plugins/mason.lua
+++ lua/oscstal/plugins/mason.lua
@@ -9,6 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             local lsp_zero = require("lsp-zero")
+            lsp_zero.extend_lspconfig()
             require("mason-lspconfig").setup({
                 handlers = {
                     lsp_zero.default_setup,
